d8265d75b2157a62b83b7787e6d3fc9d278c5e70
ofirENS <ofirshukron@gmail.com>
2014-08-08 12:35:01 +0200

    f

--- CalcDist.m
+++ CalcDist.m
@@ -1,7 +1,7 @@
 function distMat = CalcDist(vec1,vec2,T)
 % calculate the Euclidean distance between pair of observations which are
 % related by the connection matrix T, specifiying in the cell (i,j) 1, if
-% observation 1 and j are connected
+% observation 1 and j are connected 
  dim1 = size(vec1,2);
  dim2 = size(vec2,2);
  if dim1~=dim2

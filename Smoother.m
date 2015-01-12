bd1b9083c8e497f51f031376b299906ad78f03ab
ofirENS <ofirshukron@gmail.com>
2015-01-12 18:35:53 +0100

    added Smoother class to collect all signal smoothing methods

---
+++ Smoother.m
@@ -0,0 +1,41 @@
+classdef Smoother<handle
+    % Signal smoother. A class to collect all smoothing methods 
+    properties
+        method
+        signalIn
+        signalOut        
+    end
+    
+    methods
+        function obj = Smoother
+            obj.method = 'mean';
+        end        
+        
+        function sigOut = Smooth(obj,sigIn,method,span,degree)
+            obj.signalIn = sigIn;
+            if ~exist('degree','var')
+                degree = 1.5;
+            end
+            if ~exist('span','var')
+                span = 15;
+            end
+            
+            if strcmpi(method,'moving')
+                
+            elseif strcmpi(method,'lowess')
+                obj.signalOut = smooth(sigIn,span,method);
+            elseif strcmpi(method,'loess')
+                obj.signalOut = smooth(sigIn,span,method);
+            elseif strcmpi(method,'sgolay')
+                obj.signalOut = smooth(sigIn,span,method);
+            elseif strcmpi(method,'rlowess')
+                obj.signalOut = smooth(sigIn,span,method);
+            elseif strcmpi(method,'rloess')
+                obj.signalOut = smooth(sigIn,span,method);
+            elseif strcmpi(method,'gaussian')
+                obj.signalOut = conv(sigIn,fspecial('gaussian',[1,span],degree),'same');                
+            end
+            sigOut = obj.signalOut;
+        end
+    end
+end

a96dc10f278036993dada6de95f02caab626b0fd
ofirENS <ofirshukron@gmail.com>
2015-03-04 02:16:01 +0100

    bilateral filter added an environment variable for faster smoothing

--- Smoother.m
+++ Smoother.m
@@ -135,15 +135,43 @@ classdef Smoother<handle
             % Calculate a bilateral filter with Gaussian functions 
             % currently supports 1D signals only 
             signalOut = signalIn;
-            sigmaf    = 5;  %intensity relationship
-            sigmag    = 5; % spatial relationship
-            for pIdx = 1:numel(signalIn)
+            sigmaf    = 5.8;  %intensity relationship
+            sigmag    = 2; % spatial relationship
+%             [x,y]     = meshgrid(1:size(signalIn,2),1:size(signalIn,1));
+            f         = @(x,x0,sigma) (1/(sqrt(2*pi*sigma))).*exp(-(x-x0).^2 ./(2*sigma^2));
+            envRad    = 40;
+            m         = size(signalIn,1);
+            n         = size(signalIn,2);
+            for rIdx = 1:m
+                
+                for cIdx = 1:n
                 % calculate the weight for point pIdx
-                wp =  sum((1./sqrt(2*pi*sigmaf^2)).*exp(-(signalIn-signalIn(pIdx)).^2 ./(2*sigmaf^2)).*...
-                         (1./sqrt(2*pi*sigmag^2)).*exp(-((1:numel(signalIn))-pIdx).^2 ./(2*sigmag^2)));
-                signalOut(pIdx) = (1/wp) *sum(signalIn.*(1./sqrt(2*pi*sigmaf^2)).*exp(-(signalIn-signalIn(pIdx)).^2 ./(2*sigmaf^2)).*...
-                         (1./sqrt(2*pi*sigmag^2)).*exp(-((1:numel(signalIn))-pIdx).^2 ./(2*sigmag^2)));
+                mx   = min(rIdx+envRad,m);
+                Mx   = max(rIdx-envRad,1);
+                my   = min(cIdx+envRad,n);
+                My   = max(cIdx-envRad,1);
+                kx   = Mx:mx;
+                ky   = My:my;
+                env  = signalIn(kx,ky);
+                [y,x] = meshgrid(1:size(env,2),1:size(env,1));
+                centerX = rIdx-Mx+1;
+                centerY = cIdx-My+1;
+                s1   = f(env,env(centerX,centerY),sigmaf).*f(x,x(centerX,centerY),sigmag).*f(y,y(centerX,centerY),sigmag);
+                wp   = sum(s1(:)); 
+
+                s2                   = env.*s1;
+                signalOut(rIdx,cIdx) = (1/wp).*sum(s2(:));
+
+                end
             end
+%             
+%             for pIdx = 1:numel(signalIn)
+%                 wp =  sum((1./sqrt(2*pi*sigmaf^2)).*exp(-(signalIn-signalIn(pIdx)).^2 ./(2*sigmaf^2)).*...
+%                          (1./sqrt(2*pi*sigmag^2)).*exp(-((1:numel(signalIn))-pIdx).^2 ./(2*sigmag^2)));
+%                 signalOut(pIdx) = (1/wp) *sum(signalIn.*(1./sqrt(2*pi*sigmaf^2)).*exp(-(signalIn-signalIn(pIdx)).^2 ./(2*sigmaf^2)).*...
+%                          (1./sqrt(2*pi*sigmag^2)).*exp(-((1:numel(signalIn))-pIdx).^2 ./(2*sigmag^2)));
+%             end
+%             end
         end
 
     end

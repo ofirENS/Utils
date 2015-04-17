902d710d490c0205aa63634de3743fc437ebac27
ofirENS <ofirshukron@gmail.com>
2015-04-17 18:29:25 +0200

    the python files were altered

--- scrTestBrownian.py
+++ scrTestBrownian.py
@@ -7,20 +7,21 @@ Created on Tue Mar 03 19:10:46 2015
 # this is a script to test the Brownian class performance
 from Brownian import Brownian# import the Brownian class from the working folders
 import time
-from matplotlib.pyplot import show
+#from matplotlib.pyplot import show
 
 
 # class parameters 
 numTraj   = 1 # number of trajectorie to generate
-numPoints = 20000 # number of points
-alpha     = 0.9 # plot alpha
+numPoints = 200000 # number of points
+alpha     = 0.3 # plot alpha
 mSize     = 1;  # plot marker size
 
-b=Brownian()
-print(b)
+#b=Brownian()
+#print(b)
 start_time = time.time()
 b=Brownian(numTraj,numPoints)
 print("--- %s seconds ---" % (time.time() - start_time)) 
-for tIdx in range(numTraj):
-    b.trajectory[tIdx].Plot(mSize,alpha)
-show() 
+
+#for tIdx in range(numTraj):
+ #   b.trajectory[tIdx].Plot(mSize,alpha)
+#show() 
